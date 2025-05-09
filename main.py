e36f99a0ca4bc129907b9657f2de4bce748129aa
Kento Horio <horiok@iharamfg.co.jp>
2025-05-09 18:31:59 +0900

    Initial clean commit: version 0.0.1

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("ACCROAD_Subsystems　v0.0.1")
+
+st.header("リリース履歴")
+
+st.markdown("""
+### 2025/5/10
+- ベータ版として付加価値計算機能を導入しました。
+- サイドバーの`付加価値取得` から実行可能です。
+""")
